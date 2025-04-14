2f9dadfe8595ca449b60231e4514dd563e092a79
Anastasia Pankova <pankkoo89@gmail.com>
2025-04-14 13:45:39 +0300

    add interceptor and context

---
+++ Contest.Service/ContestService.DAL/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ContestService.DAL.Entities;
+using ContestService.DAL.Interceptors;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ContestService.DAL.Context
+{
+    internal class AppDbContext(DbContextOptions options, TimestampInterceptor timestampInterceptor) : DbContext(options)
+    {
+        public DbSet<Stage> Stages { get; set; }
+        public DbSet<Nomination> Nominations { get; set; }
+        public DbSet<MusicalInstrument> MusicalInstruments { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.AddInterceptors(timestampInterceptor);
+        }
+    }
+}
---
+++ Contest.Service/ContestService.DAL/Interceptors/TimestampInterceptor.cs
@@ -0,0 +1,39 @@
+﻿using ContestService.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace ContestService.DAL.Interceptors
+{
+    internal class TimestampInterceptor : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            UpdateTimestamps(eventData);
+            return base.SavingChanges(eventData, result);
+        }
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken ct)
+        {
+            UpdateTimestamps(eventData);
+            return base.SavingChangesAsync(eventData, result, ct);
+        }
+
+        public void UpdateTimestamps(DbContextEventData eventData)
+        {
+            var context = eventData.Context;
+            var utcNow = DateTime.UtcNow;
+
+            foreach (var entry in eventData.Context.ChangeTracker.Entries<IHasTimestamps>())
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreatedAt = utcNow;
+                    entry.Entity.UpdatedAt = utcNow;
+                }
+                else if (entry.State == EntityState.Modified)
+                {
+                    entry.Entity.UpdatedAt = utcNow;
+                }
+            }
+        }
+    }
+}
