03a2e36063ba102ff20ce2e6cb30a0e43bd178c9
Anastasia Pankova <pankkoo89@gmail.com>
2025-04-14 13:47:01 +0300

    add dependency injection

---
+++ Contest.Service/ContestService.BLL/DI/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContestService.DAL.DI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContestService.BLL.DI
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddBllServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDalDependencies(configuration);
+
+            
+
+            return services;
+        }
+    }
+}
---
+++ Contest.Service/ContestService.DAL/DI/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using ContestService.DAL.Context;
+using ContestService.DAL.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContestService.DAL.DI
+{
+    public  static class DependencyInjection
+    {
+        public static IServiceCollection AddDalDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AppDbContext>(options =>
+                    options.UseNpgsql(connectionString, npgsqlOptions =>
+                        npgsqlOptions.EnableRetryOnFailure(
+                                        maxRetryCount: 5,
+                                        maxRetryDelay: TimeSpan.FromSeconds(10),
+                                        errorCodesToAdd: null))
+                                        .AddInterceptors(new TimestampInterceptor()));
+
+           
+
+            return services;
+
+        }
+    }
+}
