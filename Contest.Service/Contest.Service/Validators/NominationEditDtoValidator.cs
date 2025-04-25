5a251a6f64b3ecc3d2cdfe784dd3b10f1d790bce
Anastasia Pankova <pankkoo89@gmail.com>
2025-04-25 13:44:32 +0300

    Add Validation

---
+++ Contest.Service/Contest.Service/Validators/NominationEditDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using ContestService.API.DTO.NominationDtos;
+using FluentValidation;
+
+namespace ContestService.API.Validators;
+
+public class NominationEditDtoValidator : AbstractValidator<NominationEditDto>
+{
+    public NominationEditDtoValidator()
+    {
+        RuleFor(model => model.Name).NotEmpty().WithMessage("Nomination name is required")
+            .Length(2, 50);
+    }
+}
---
+++ Contest.Service/Contest.Service/Validators/ParticipantEditDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using ContestService.API.DTO.ParticipantDtos;
+using FluentValidation;
+
+namespace ContestService.API.Validators;
+
+public class ParticipantEditDtoValidator : AbstractValidator<ParticipantEditDto>
+{
+    public ParticipantEditDtoValidator()
+    {
+        RuleFor(model => model.Name).NotEmpty().WithMessage("Name is required.")
+            .Length(2, 50).WithMessage("Name must be between 2 and 50 characters.");
+
+        RuleFor(model => model.Surname).NotEmpty().WithMessage("Surname is required.")
+            .Length(2, 50).WithMessage("Surname must be between 2 and 50 characters.");
+
+        RuleFor(model => model.Birthday).Must(birthday =>
+        {
+            var today = DateOnly.FromDateTime(DateTime.Today);
+            var age = today.Year - birthday.Year;
+            if (birthday > today.AddYears(-age)) age--;
+            return age >= 18 && age <= 120;
+        })
+            .WithMessage("Age must be between 18 and 120 years.");
+    }
+}
