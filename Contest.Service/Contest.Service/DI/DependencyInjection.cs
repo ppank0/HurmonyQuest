548cc1750b7dfb544e5517ba982b09b02cc2ab5a
Anastasia Pankova <pankkoo89@gmail.com>
2025-04-25 13:44:48 +0300

    Add di

---
+++ Contest.Service/Contest.Service/DI/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using ContestService.API.Mapper;
+using FluentValidation.AspNetCore;
+using FluentValidation;
+using System.Reflection;
+using ContestService.API.Validators;
+
+namespace ContestService.API.DI;
+
+public static class DependencyInjection
+{
+    public static void AddApiServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAutoMapper(typeof(MappingProfile));
+        services.AddAutoValidation();
+
+    }
+    public static void AddAutoValidation(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddFluentValidationAutoValidation();
+    }
+}
