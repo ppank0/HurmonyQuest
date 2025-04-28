64f11b9793e2f6e4955a0ebc4d61e243dc1edf19
Anastasia Pankova <90717539+ppank0@users.noreply.github.com>
2025-04-28 15:02:05 +0300

    add middleware (#28)
    * add middleware
    
    * Simplify ExceptionMiddleware
    
    * simplify exception handling

---
+++ Contest.Service/Contest.Service/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using ContestService.BLL.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ContestService.API.Middleware;
+
+public class ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+{
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            await HandleException(httpContext, ex);
+        }
+    }
+
+    private async Task HandleException(HttpContext context, Exception ex)
+    {
+        logger.LogError(ex, "Unhandle exception");
+
+        var (statusCode, message) = ex switch
+        {
+            NotFoundException => (HttpStatusCode.NotFound, ex.Message),
+            BadRequestException => (HttpStatusCode.BadRequest, ex.Message),
+            _ => (HttpStatusCode.InternalServerError, ex.Message)
+        };
+
+        ProblemDetails details = new()
+        {
+            Status = (int)statusCode,
+            Title = ex.Message,
+        };
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = details.Status.Value;
+
+        await context.Response.WriteAsJsonAsync(details);
+    }
+}
