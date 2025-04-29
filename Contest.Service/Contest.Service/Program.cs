2eda734c822e9901590f5f8f90f3a746ec4b3aa6
Anastasia Pankova <90717539+ppank0@users.noreply.github.com>
2025-04-29 14:55:59 +0300

    add logging via serilog (#29)

--- Contest.Service/Contest.Service/Program.cs
+++ Contest.Service/Contest.Service/Program.cs
@@ -1,12 +1,16 @@
 using ContestService.API.DI;
 using ContestService.API.Middleware;
 using ContestService.BLL.DI;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host.UseSerilog((context, services, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
 
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,6 +19,8 @@ builder.Services.AddApiServices(builder.Configuration);
 builder.Services.AddBllServices(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 app.UseMiddleware<ExceptionMiddleware>();
 
