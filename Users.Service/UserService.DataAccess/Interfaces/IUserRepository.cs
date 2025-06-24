8c61ab490e5818f7240e196bf6052f3843b4c00a
Anastasia Pankova <pankkoo89@gmail.com>
2025-06-24 18:54:06 +0300

    creatе a project structure

---
+++ Users.Service/UserService.DataAccess/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using UserService.DataAccess.Entities;
+
+namespace UserService.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task AddAsync(UserEntity user);
+        Task<UserEntity?> GetByIdAsync(Guid id);
+        Task<IEnumerable<UserEntity>> GetAllAsync();
+        Task DeleteAsync(Guid id);
+    }
+}
