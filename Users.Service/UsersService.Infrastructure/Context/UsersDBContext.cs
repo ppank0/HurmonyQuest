517e781222384feb5d2f8a7b595ad6b8c512412f
Anastasia Pankova <pankkoo89@gmail.com>
2025-06-26 12:16:12 +0300

    start implementing infrastructure

---
+++ Users.Service/UsersService.Infrastructure/Context/UsersDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserService.DataAccess.Entities;
+
+namespace UsersService.Infrastructure.Context
+{
+    public class UsersDBContext : DbContext
+    {
+        public UsersDBContext(DbContextOptions options) : base(options) => Database.Migrate();
+
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
