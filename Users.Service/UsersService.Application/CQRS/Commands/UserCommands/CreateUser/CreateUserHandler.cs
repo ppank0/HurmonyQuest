dd714ad31db95a5c087011f773a8d3bcc01f44f4
Anastasia Pankova <pankkoo89@gmail.com>
2025-07-02 16:45:45 +0300

    add dtos and cqrs command part

---
+++ Users.Service/UsersService.Application/CQRS/Commands/UserCommands/CreateUser/CreateUserHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using UsersService.Domain.Entities;
+using UsersService.Domain.Interfaces;
+using UsersService.Application.DTOs;
+using UsersService.Domain.Exceptions;
+
+namespace UsersService.Application.CQRS.Commands.UserCommands.CreateUser
+{
+    public class CreateUserHandler(IUserRepository repository) : IRequestHandler<CreateUserCommand, UserDto>
+    {
+
+        public async Task<UserDto> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            var existing = await repository.GetByAuthIdAsync(request.UserDto.AuthId);
+
+            if(existing is not null)
+            {
+                throw new BadRequestException("User already exists");
+            }
+
+            var user = new UserEntity
+            {
+                Email = request.UserDto.Email,
+                UserPictureUrl = request.UserDto.UserPictureUrl,
+                AuthId = request.UserDto.AuthId,
+            }; 
+
+            await repository.AddAsync(user);
+
+            return new UserDto
+            (
+                user.Id,
+                user.Email,
+                user.UserPictureUrl,
+                user.AuthId
+            );
+        }
+    }
+}
---
+++ Users.Service/UsersService.Application/CQRS/Commands/UserCommands/UpdateUser/UpdateUserHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using UsersService.Application.DTOs;
+using UsersService.Domain.Exceptions;
+using UsersService.Domain.Interfaces;
+
+namespace UsersService.Application.CQRS.Commands.UserCommands.UpdateUser
+{
+    public class UpdateUserHandler(IUserRepository repository) : IRequestHandler<UpdateUserCommand, UserDto>
+    {
+        public async Task<UserDto> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var userToUpdate = await repository.GetByIdAsync(request.Id);
+
+            if (userToUpdate is null)
+            {
+                throw new NotFoundException(request.Id);
+            }
+
+            userToUpdate.UserPictureUrl = request.UserDto.UserPictureUrl;
+
+            var updatedUser = await repository.UpdateAsync(userToUpdate);
+
+            return new UserDto(updatedUser.Id, updatedUser.Email, updatedUser.UserPictureUrl, updatedUser.AuthId);
+        }
+    }
+}
