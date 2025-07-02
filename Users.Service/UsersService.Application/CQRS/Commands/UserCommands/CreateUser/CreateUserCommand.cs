061886036702e15c0727f555a952bca9cb471cbe
Anastasia Pankova <pankkoo89@gmail.com>
2025-07-02 16:46:23 +0300

    add needed createUserCommand

---
+++ Users.Service/UsersService.Application/CQRS/Commands/UserCommands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using UsersService.Application.DTOs;
+
+
+namespace UsersService.Application.CQRS.Commands.UserCommands.CreateUser
+{
+    public record CreateUserCommand(CreateUserDto UserDto) : IRequest<UserDto>
+    {
+    }
+}
