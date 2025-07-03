22866dae88dd230f2d99543b0aecd33a54c1c517
Anastasia Pankova <pankkoo89@gmail.com>
2025-07-03 16:39:05 +0300

    add connection to db

---
+++ Users.Service/Users.Service/Extensions/ServicesConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsersService.Infrastructure.Context;
+
+namespace UsersService.API.Extensions
+{
+    public static class ServicesConfiguration
+    {
+        public static void CongigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<UsersDBContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"), x => x.MigrationsAssembly("UsersService.Infrastructure")));
+    }
+}
