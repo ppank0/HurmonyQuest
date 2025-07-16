191fdb4d29bcc3aaaca572d73aa4217a1ae3f8ce
Anastasia Pankova <pankkoo89@gmail.com>
2025-07-16 17:02:05 +0300

    add unit tests

---
+++ Users.Service/UsersService.Tests/Mapping/UsersProfileTest.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using UsersService.Application.DTOs;
+using UsersService.Domain.Entities;
+
+namespace UsersService.Tests.Mapping
+{
+    internal class UsersProfileTest : Profile
+    {
+        public UsersProfileTest()
+        {
+            CreateMap<UserEntity, UserDto>();
+            CreateMap<CreateUserDto, UserEntity>();
+        }
+    }
+}
