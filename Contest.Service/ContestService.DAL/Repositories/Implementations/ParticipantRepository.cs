aad28a5d34f0d461279ed6d473ccaf9e8b7e789f
haharynP <90717539+ppank0@users.noreply.github.com>
2025-10-27 15:10:45 +0100

    fix sonar issue

--- Contest.Service/ContestService.DAL/Repositories/Implementations/ParticipantRepository.cs
+++ Contest.Service/ContestService.DAL/Repositories/Implementations/ParticipantRepository.cs
@@ -2,18 +2,19 @@
 using ContestService.DAL.Entities;
 using ContestService.DAL.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ContestService.DAL.Repositories.Implementations;
-public class ParticipantRepository(AppDbContext context) : RepositoryBase<Participant>(context), IParticipantRepository
+
+public class ParticipantRepository : RepositoryBase<Participant>, IParticipantRepository
 {
+    private readonly AppDbContext _context;
+    public ParticipantRepository(AppDbContext context): base(context)
+    {
+        _context = context;
+    }
     public async Task<List<Participant>> GetAllWithRelationsAsync(CancellationToken cancellationToken)
     {
-        return await context.Participants
+        return await _context.Participants
             .Include(p => p.MusicalInstrument)
             .ToListAsync(cancellationToken);
     }
