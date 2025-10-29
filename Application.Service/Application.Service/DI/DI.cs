ec736ce328834136941514927dddeacd3d746589
haharynP <90717539+ppank0@users.noreply.github.com>
2025-10-29 15:20:14 +0100

    config di

---
+++ Application.Service/Application.Service/DI/DI.cs
@@ -0,0 +1,13 @@
+﻿using ApplicationService.BLL.DI;
+
+namespace Application.Service.DI
+{
+    public static class DI
+    {
+        public static IServiceCollection AddApiDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddBLLDependencies(configuration);
+            return services;
+        }
+    }
+}
