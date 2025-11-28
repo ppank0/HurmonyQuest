72ba81c0b4cf4864f78eef818e5f3559e2602a71
ppank0 <pankkoo89@gmail.com>
2025-11-28 15:15:45 +0100

    add video processing service via MinIO

---
+++ Application.Service/ApplicationService.Bll/Options/MinioOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace ApplicationService.BLL.Options
+{
+    public sealed class MinioOptions
+    {
+        public string Endpoint { get; set; } = string.Empty;
+        public string AccessKey { get; set; } = string.Empty;
+        public string SecretKey { get; set; } = string.Empty;
+    }
+}
---
+++ Application.Service/ApplicationService.Bll/Services/VideoService.cs
@@ -0,0 +1,54 @@
+﻿using ApplicationService.BLL.Interfaces;
+using ApplicationService.BLL.Models;
+using ApplicationService.DAL.Entities;
+using ApplicationService.DAL.UnitOfWork;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using static ApplicationService.BLL.Consts.MinIOConsts;
+
+namespace ApplicationService.BLL.Services
+{
+    public class VideoService(IUnitOfWork uOw, IMapper mapper, IVideoStorage storage) : IVideoService
+    {
+        private readonly IUnitOfWork _uOw = uOw;
+        private readonly IMapper _mapper = mapper;
+        private readonly IVideoStorage _videoStorage = storage;
+
+        public Task<List<VideoModel>> GetAllAsync(CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<(Stream, string)> GetAsync(string videoName, CancellationToken ct)
+        {
+            return await _videoStorage.GetObjectAsync(BucketName, videoName, ct);
+        }
+
+        public async Task<VideoModel> PutAsync(string objName, string contentType, Stream data, CancellationToken ct)
+        {
+            await _videoStorage.EnsureBucketExists(BucketName, ct);
+            await _videoStorage.PutObjectAsync(BucketName, objName, contentType, data, ct);
+            var videoModel = new VideoModel() { VideoUrl = objName };
+            var video = await _uOw.Videos.CreateAsync(_mapper.Map<VideoEntity>(videoModel), ct);
+
+            await _uOw.SaveAsync(ct);
+
+            return _mapper.Map<VideoModel>(video);
+        }
+
+        public async Task DeleteAsync(Guid id, CancellationToken ct)
+        {
+            var videoModelList = _uOw.Videos.FindByCondition((s => s.Id == id), ct);
+            var videoModel = await videoModelList.FirstOrDefaultAsync();
+            if (videoModel is null)
+            {
+                throw new NullReferenceException();
+            }
+
+            await _videoStorage.DeleteObjectAsync(BucketName, videoModel.VideoUrl, ct);
+            await _uOw.Videos.DeleteAsync((_mapper.Map<VideoEntity>(videoModel)), ct);
+
+            await _uOw.SaveAsync(ct);
+        }
+    }
+}
