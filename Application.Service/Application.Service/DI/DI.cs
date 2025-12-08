6d0b0f33c311c3416fd5158dc26e2b81944ca0d8
ppank0 <pankkoo89@gmail.com>
2025-12-08 10:49:10 +0100

    add controllers, configure di, options

--- Application.Service/Application.Service/DI/DI.cs
+++ Application.Service/Application.Service/DI/DI.cs
@@ -1,4 +1,10 @@
-﻿using ApplicationService.BLL.DI;
+﻿using Application.Service.HttpClients;
+using Application.Service.Mapper;
+using Application.Service.Options;
+using ApplicationService.BLL.DI;
+using ApplicationService.BLL.Integrations.Contracts.Instruments;
+using ApplicationService.BLL.Integrations.Contracts.Participant;
+using Microsoft.Extensions.Options;
 
 namespace Application.Service.DI
 {
@@ -6,7 +12,30 @@ namespace Application.Service.DI
     {
         public static IServiceCollection AddApiDependencies(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddAutoMapper(typeof(MapperProfile));
             services.AddBLLDependencies(configuration);
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ApplicationService.BLL.DI.DI).Assembly));
+            services.Configure<ParticipantsOptions>(configuration.GetSection("Participants"));
+            services.Configure<InstrumentsOptions>(configuration.GetSection("Instruments"));
+
+            services.AddHttpClient<IParticipantHttpClient, ParticipantHttpClient>((sp, http) =>
+            {
+                var opt = sp.GetRequiredService<IOptions<ParticipantsOptions>>().Value;
+                http.BaseAddress = new Uri(opt.BaseUrl);
+                http.Timeout = Timeout.InfiniteTimeSpan;
+                if (!string.IsNullOrWhiteSpace(opt.ApiKey))
+                    http.DefaultRequestHeaders.Add("X-Api-Key", opt.ApiKey);
+            });
+
+            services.AddHttpClient<IInstrumentHttpClient, InstrumentHttpClient>((sp, http) =>
+            {
+                var opt = sp.GetRequiredService<IOptions<InstrumentsOptions>>().Value;
+                http.BaseAddress = new Uri(opt.BaseUrl);
+                http.Timeout = Timeout.InfiniteTimeSpan;
+                if (!string.IsNullOrWhiteSpace(opt.ApiKey))
+                    http.DefaultRequestHeaders.Add("X-Api-Key", opt.ApiKey);
+            });
+
             return services;
         }
     }
---
+++ Application.Service/Application.Service/HttpClients/ParticipantHttpClient.cs
@@ -0,0 +1,42 @@
+﻿using ApplicationService.BLL.Exceptions;
+using ApplicationService.BLL.Integrations.Contracts.Participant;
+using ApplicationService.BLL.Integrations.Contracts.Participants.DTOs;
+
+namespace Application.Service.HttpClients
+{
+    public class ParticipantHttpClient(HttpClient httpClient) : IParticipantHttpClient
+    {
+        private readonly HttpClient _httpClient = httpClient;
+
+        public async Task<ParticipantResponse> CreateAsync(ParticipantCreateRequest req, CancellationToken ct)
+        {
+            var res = await _httpClient.PostAsJsonAsync("api/participants", req, ct);
+            res.EnsureSuccessStatusCode();
+            var data = await res.Content.ReadFromJsonAsync<ParticipantResponse>(ct);
+            if (data is null)
+            {
+                throw new InvalidOperationException("Empty response from participants service");
+            }
+            
+            return data;
+        }
+
+        public async Task<ParticipantResponse> GetAsync(Guid id, CancellationToken ct)
+        {
+            var res = await _httpClient.GetAsync($"api/participants/{id}", ct);
+            res.EnsureSuccessStatusCode();
+            var data = await res.Content.ReadFromJsonAsync<ParticipantResponse>(ct);
+            if (data is null)
+            {
+                throw new InvalidResponseException("Empty response from participants service");
+            }
+            return data;
+        }
+
+        public async Task DeleteAsync(Guid id, CancellationToken ct)
+        {
+            var res = await _httpClient.DeleteAsync($"api/participants/{id}", ct);
+            res.EnsureSuccessStatusCode();
+        }
+    }
+}
