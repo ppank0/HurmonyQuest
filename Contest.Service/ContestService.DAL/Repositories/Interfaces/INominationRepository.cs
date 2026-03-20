7aaeea3ec30c02a92e0c9f380364f39e33e5bcce
ppank0 <pankkoo89@gmail.com>
2026-03-20 15:31:38 +0100

    change auth using duende & remove redundant code

--- Contest.Service/ContestService.DAL/Repositories/Interfaces/INominationRepository.cs
+++ Contest.Service/ContestService.DAL/Repositories/Interfaces/INominationRepository.cs
@@ -4,5 +4,4 @@ namespace ContestService.DAL.Repositories.Interfaces;
 public interface INominationRepository : IRepositoryBase<Nomination>
 {
     public bool IsMusicalInstrumentInNomination(Guid NominationId, Guid MusicalInstrumentId);
-    
 }
