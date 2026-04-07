0ede498ad3306ef54b7cd34b1172e9df32a95ef6
ppank0 <pankkoo89@gmail.com>
2026-04-07 19:34:55 +0200

    add contestService layer (api, service, interfaces)

--- hq-frontend/src/App.tsx
+++ hq-frontend/src/App.tsx
@@ -1,32 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { AppRouter} from './app/router/AppRouter';
+import { Header } from './layouts/Header/Header';
+import { Footer } from './layouts/Footer/Footer';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './shared/theme/theme';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Box } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box sx={{display: 'flex', flexDirection: 'column', minHeight: '100vh'}}>
+          <Header/>
+            <Box sx={{flexGrow: 1}}>
+              <AppRouter/>
+            </Box>
+          <Footer/>
+        </Box>
+      </ThemeProvider>
     </>
   )
 }
